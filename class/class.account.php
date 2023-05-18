f56030287886c6b8e89a4907f125cfba9a9b2432
jaldrinportugal <portugaljohnaldrin30@gmail.com>
2023-05-18 09:47:45 +0800

    aa

---
+++ class/class.account.php
@@ -0,0 +1,29 @@
+<?php
+ 
+class Accounts{
+    public function login(){
+        session_start();
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $pdo = new PDO('mysql:host=localhost;dbname=chatroom','root','');
+
+        $account = $pdo->prepare('SELECT * FROM accounts WHERE email=:email and password=:password');
+        $account->bindValue('email',$email);
+        $account->bindValue('password',$password);
+        $account->execute();
+
+        $data = $account->fetchAll(PDO::FETCH_ASSOC);
+
+        if(count ($data) == 1){
+            echo 'success';
+            $_SESSION['auth'] = $data;
+        }else{
+            echo 'error';
+        }
+    }
+
+    public function register(){
+        
+    }
+}
